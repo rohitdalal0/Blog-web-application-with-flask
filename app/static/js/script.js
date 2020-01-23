4435d4fad166ed80f2edde16b0e88ad52deb5de5
Rohit <32985298+Rohit-Dalal@users.noreply.github.com>
2020-01-23 15:53:20 +0530

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,18 +4,30 @@ $(document).ready(function () {
     let status = $("#thumb-up").attr('class');
     if (status == 'fa fa-thumbs-o-up'){
       $("#thumb-up").removeClass('fa fa-thumbs-o-up').addClass('fa fa-thumbs-up');
+      var p_id = $("#post_id").attr('class');
+      like_update('fa fa-thumbs-up', p_id);
       
     }else{
       $("#thumb-up").removeClass('fa fa-thumbs-up').addClass('fa fa-thumbs-o-up');
+      var _id = $("#post_id").attr('class');
+      like_update('fa fa-thumbs-o-up', _id);
+    };
+
+    function like_update(state, post_id){
+      $.ajax({
+          url: '/like_update',
+          method: 'POST',
+          data : {
+            'state': state,
+            'id': post_id,
+          },
+
+          success: (data)=>{
+            $('#total_like').fadeOut(200).text(data.total_like).fadeIn(200);
+          },
+
+      });
     };
 
   });
-  //  I need to create a triger in 'if statment' which triger a function to call 
-  // ajax and ajax send data to python, it's handle backend query. after procedure
-  // 'data' by ajax, It's sendback to JQuery and JQuery show data in the html page but real time.
-
-  
-
-
-
   });
